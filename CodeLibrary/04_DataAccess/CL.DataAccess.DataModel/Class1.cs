5567194a0041403f59108fcbc57c8b18278b9580
25717594@qq.com <25717594@qq.com>
2015-02-24 13:01:54 +0800

    submit

---
+++ CodeLibrary/04_DataAccess/CL.DataAccess.DataModel/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CL.DataAccess.DataModel
+{
+    public class Class1
+    {
+    }
+}
