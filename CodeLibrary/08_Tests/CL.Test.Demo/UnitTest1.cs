923310dbedcc898cd653136e754356f33606c2a9
25717594@qq.com <25717594@qq.com>
2015-02-25 22:02:23 +0800

    Add test project

---
+++ CodeLibrary/08_Tests/CL.Test.Demo/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CL.Test.Demo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string a = "21";
+            Assert.AreEqual("211", a);
+        }
+    }
+}
--- CodeLibrary/09_Framework/CL.Framework.Utils/Common/EnumUtil.cs
+++ CodeLibrary/09_Framework/CL.Framework.Utils/Common/EnumUtil.cs
@@ -33,29 +33,7 @@ namespace CL.Framework.Utils
         }
 
 
-        /// <summary>
-        /// 获取指定枚举的Description信息
-        /// </summary>
-        /// <param name="e">枚举值(T为枚举类型；e为枚举值）</param>
-        /// <returns>获取成功返回描述；获取失败返回空字符串</returns>
-        public static string GetEnumDescription<T>(T e)
-        {
-            FieldInfo fielInfo = e.GetType().GetField(e.ToString());
-            if (fielInfo == null) return "";
-
-            object[] attrs = fielInfo.GetCustomAttributes(true);
-            if (attrs.Length <= 0)
-            {
-                return string.Empty;
-            }
-
-            DescriptionAttribute desAttr = attrs[0] as DescriptionAttribute;
-            return desAttr.Description;
-
-            //MenuTypeEnum enumType = ((MenuTypeEnum)e.CellValue.ToInt());  将枚举值转换为枚举类型，如 1=>EnumUtil.Query
-            //e.Cell.Text = EnumHelper.GetEnumDescription<MenuTypeEnum>(enumType);
-        }
+     
     }
-
 
 }
