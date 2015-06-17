48607cde53a135316703f0dbb91f3b95e1767b9a
JamesFoxChen <25717594@qq.com>
2015-06-17 17:36:58 +0800

    add GetDescription() and GetRequestMsgType() method

--- CodeLibrary/09_Framework/CL.Framework.Utils/Common/EnumUtil.cs
+++ CodeLibrary/09_Framework/CL.Framework.Utils/Common/EnumUtil.cs
@@ -9,6 +9,35 @@ namespace CL.Framework.Utils
 {
     public class EnumUtil
     {
+
+//字符串转换成枚举值
+   public static RequestMsgType GetRequestMsgType(string str)
+        {
+            return (RequestMsgType)Enum.Parse(typeof(RequestMsgType), str, true);
+        }
+        
+//获取枚举描述属性
+ public static string GetDescription(Enum en)
+        {
+            Type type = en.GetType();
+
+            MemberInfo[] memInfo = type.GetMember(en.ToString());
+
+            if (memInfo != null && memInfo.Length > 0)
+            {
+                object[] attrs = memInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                if (attrs != null && attrs.Length > 0)
+                {
+                    return ((DescriptionAttribute)attrs[0]).Description;
+                }
+            }
+
+            return en.ToString();
+        }
+    }
+
+
         /// <summary>
         /// 数据库操作类型
         /// </summary>
@@ -31,9 +60,5 @@ namespace CL.Framework.Utils
             /// </summary>
             Delete
         }
-
-
-     
-    }
 
 }
