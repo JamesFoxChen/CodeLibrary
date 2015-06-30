026c7e1e0049046f2ed13556d5b7e184760a0f9b
JamesFoxChen <25717594@qq.com>
2015-06-30 18:02:31 +0800

    add

--- CodeLibrary/02_Services/CL.Services.WinService/TopshelfService.cs
+++ CodeLibrary/02_Services/CL.Services.WinService/TopshelfService.cs
@@ -14,7 +14,12 @@ namespace CL.Services.WinService
         {
             TextLoggingService.Error("不要关本窗体，谢谢-陈志");
             writeIISLogToSqlserver();
-            executeLogDataStart();
+            deleteHistoryRecord();
+            deleteRepeatRecord();
+            executeRecord();
+
+            //var dt = AdoMng.Get("SELECT * FROM IISLog_table");
+            //deleteRepeatRecordInner();
         }
 
         public void Stop()
@@ -23,13 +28,15 @@ namespace CL.Services.WinService
 
 
         #region 执行写入IIS日志写入数据库日志
-
+        /// <summary>
+        /// 执行写入IIS日志写入数据库日志
+        /// </summary>
         private void writeIISLogToSqlserver()
         {
             try
-            {       
+            {
                 //每十分钟操作一次
-                var interval = Convert.ToInt32(ConfigurationManager.AppSettings["Log_Interval"]);
+                var interval = Convert.ToInt32(ConfigurationManager.AppSettings["Log_ReadLogAndWriteToDBInterval"]);
                 var mt = new Timer(interval);
                 mt.Enabled = true;
                 mt.Elapsed += (o, e) =>
@@ -55,36 +62,36 @@ namespace CL.Services.WinService
         /// </summary>
         private void sendCmd()
         {
-                string locationCmd = ConfigurationManager.AppSettings["Log_LogParsePath"]; //@"cd C:\Program Files (x86)\Log Parser 2.2\";
+            string locationCmd = ConfigurationManager.AppSettings["Log_LogParsePath"]; //@"cd C:\Program Files (x86)\Log Parser 2.2\";
 
-                string select = ConfigurationManager.AppSettings["Log_Select"];
-                //"SELECT TO_LOCALTIME(TO_TIMESTAMP(ADD(TO_STRING(date, 'yyyy-MM-dd '), TO_STRING(time, 'hh:mm:ss')),'yyyy-MM-dd hh:mm:ss')) AS RequestTime";
+            string select = ConfigurationManager.AppSettings["Log_Select"];
+            //"SELECT TO_LOCALTIME(TO_TIMESTAMP(ADD(TO_STRING(date, 'yyyy-MM-dd '), TO_STRING(time, 'hh:mm:ss')),'yyyy-MM-dd hh:mm:ss')) AS RequestTime";
 
-                string fileDirPath = ConfigurationManager.AppSettings["Log_FileDirPath"];// @"C:\";
-                string filePath = fileDirPath + "u_ex" + DateTime.Now.ToString("yyMMddHH") + ".log";  //u_ex150630.log  u_ex15063013
-                if (!File.Exists(filePath)) return;
+            string fileDirPath = ConfigurationManager.AppSettings["Log_FileDirPath"];// @"C:\";
+            string filePath = fileDirPath + "u_ex" + DateTime.Now.ToString("yyMMddHH") + ".log";  //u_ex150630.log  u_ex15063013
+            if (!File.Exists(filePath)) return;
 
-                //如果读日志影响到写日志操作，则将日志复制成临时文件，并临时文件读日志
-                //string tempFilePath = fileDirPath + "temp.log";
-                //if (File.Exists(tempFilePath))
-                //{
-                //    File.Delete(tempFilePath);
-                //}
-                //File.Copy(filePath,tempFilePath);
+            //如果读日志影响到写日志操作，则将日志复制成临时文件，并临时文件读日志
+            //string tempFilePath = fileDirPath + "temp.log";
+            //if (File.Exists(tempFilePath))
+            //{
+            //    File.Delete(tempFilePath);
+            //}
+            //File.Copy(filePath,tempFilePath);
 
-                string tableName = ConfigurationManager.AppSettings["Log_TableName"];//"IISLog_table";
-                string where = ConfigurationManager.AppSettings["Log_Where"];//"WHERE sc-status<>200";
-                string server = ConfigurationManager.AppSettings["Log_Server"];// "127.0.0.1";
-                string dbConfig = ConfigurationManager.AppSettings["Log_DBConfig"];//"database:Log_IIS -username:sa -password:123";
-                string sqlserverCmd = @"logparser ""{0},* FROM {1} TO {2} {3}"" -o:SQL -server:{4} -driver:""SQL Server"" -{5} -createtable:ON";
+            string tableName = ConfigurationManager.AppSettings["Log_TableName"];//"IISLog_table";
+            string where = ConfigurationManager.AppSettings["Log_Where"];//"WHERE sc-status<>200";
+            string server = ConfigurationManager.AppSettings["Log_Server"];// "127.0.0.1";
+            string dbConfig = ConfigurationManager.AppSettings["Log_DBConfig"];//"database:Log_IIS -username:sa -password:123";
+            string sqlserverCmd = @"logparser ""{0},* FROM {1} TO {2} {3}"" -o:SQL -server:{4} -driver:""SQL Server"" -{5} -createtable:ON";
 
 
-                sqlserverCmd = string.Format(sqlserverCmd, select, filePath, tableName, where, server, dbConfig);
+            sqlserverCmd = string.Format(sqlserverCmd, select, filePath, tableName, where, server, dbConfig);
 
-                TextLoggingService.Debug(locationCmd);
-                TextLoggingService.Debug(sqlserverCmd);
+            //TextLoggingService.Debug(locationCmd);
+            //TextLoggingService.Debug(sqlserverCmd);
 
-                executeCMD(locationCmd, sqlserverCmd);
+            executeCMD(locationCmd, sqlserverCmd);
         }
 
         /// <summary>
@@ -112,8 +119,8 @@ namespace CL.Services.WinService
             //向标准输入写入要执行的命令。这里使用&是批处理命令的符号，表示前面一个命令不管是否执行成功都执行后面(exit)命令，如果不执行exit命令，后面调用ReadToEnd()方法会假死
             //同类的符号还有&&和||前者表示必须前一个命令执行成功才会执行后面的命令，后者表示必须前一个命令执行失败才会执行后面的命令
 
-            //String StrInfo = p.StandardOutput.ReadToEnd();
-            //Console.WriteLine(StrInfo);
+            String StrInfo = p.StandardOutput.ReadToEnd();
+            Console.WriteLine(StrInfo);
 
             p.WaitForExit();//等待程序执行完退出进程
             p.Close();
@@ -121,13 +128,87 @@ namespace CL.Services.WinService
 
         #endregion
 
-
-        #region 处理数据库信息
-        private void executeLogDataStart()
+        #region 删除重复数据
+        /// <summary>
+        /// 删除重复数据
+        /// </summary>
+        private void deleteRepeatRecord()
         {
             try
             {
-                System.Threading.Thread t = new System.Threading.Thread(executeLogDataInner);
+                //每十分钟操作一次
+                var interval = Convert.ToInt32(ConfigurationManager.AppSettings["Log_DeleteRepeatRecordInterval"]);
+                var mt = new Timer(interval);
+                mt.Enabled = true;
+                mt.Elapsed += (o, e) =>
+                {
+                    var t = (Timer)o;
+                    t.AutoReset = false;
+                    t.Enabled = false;
+                    t.Stop();
+                    deleteRepeatRecordInner();
+                    t.Enabled = true;
+                    t.AutoReset = true;
+                    t.Start();
+                };
+            }
+            catch (Exception ex)
+            {
+                TextLoggingService.Error(ex.Message);
+            }
+        }
+
+        private void deleteRepeatRecordInner()
+        {
+            string sql = @"DELETE FROM [Log_IIS].[dbo].[IISLog_table]
+                            WHERE id not in 
+                            (select MAX(Id) from [IISLog_table] group by [RequestTime]
+                                  ,[LogFilename]
+                                  ,[LogRow]
+                                  ,[date]
+                                  ,[time]
+                                  ,[cIp]
+                                  ,[csUsername]
+                                  ,[sSitename]
+                                  ,[sComputername]
+                                  ,[sIp]
+                                  ,[sPort]
+                                  ,[csMethod]
+                                  ,[csUriStem]
+                                  ,[csUriQuery]
+                                  ,[scStatus]
+                                  ,[scSubstatus]
+                                  ,[scWin32Status]
+                                  ,[scBytes]
+                                  ,[csBytes]
+                                  ,[timeTaken]
+                                  ,[csVersion]
+                                  ,[csHost]
+                                  ,[csUserAgent]
+                                  ,[csCookie]
+                                  ,[csReferer]
+                                  ,[sEvent]
+                                  ,[sProcessType]
+                                  ,[sUserTime]
+                                  ,[sKernelTime]
+                                  ,[sPageFaults]
+                                  ,[sTotalProcs]
+                                  ,[sActiveProcs]
+                                  ,[sStoppedProcs] having count(*) > 1)";
+            AdoMng.Execute(sql);
+        }
+
+        #endregion
+
+        #region 删除历史数据
+        /// <summary>
+        /// 删除历史数据
+        /// </summary>
+        private void deleteHistoryRecord()
+        {
+            try
+            {
+                System.Threading.Thread t = new System.Threading.Thread(deleteHistoryRecordInner);
                 t.Name = "ExecuteLogDataStart";
                 t.Start();
             }
@@ -137,10 +218,7 @@ namespace CL.Services.WinService
             }
         }
 
-        //操作日志
-        //定期清理日志记录
-        //根据日志信息发送邮件
-        private void executeLogDataInner()
+        private void deleteHistoryRecordInner()
         {
             while (true)
             {
@@ -150,11 +228,47 @@ namespace CL.Services.WinService
                 }
                 else
                 {
-                    TextLoggingService.Debug(DateTime.Now.ToString("HHmmss"));
+                    //TextLoggingService.Debug(DateTime.Now.ToString("HHmmss"));
                     System.Threading.Thread.Sleep(1000);
                 }
             }
         }
+        #endregion
+
+        #region 处理数据
+        /// <summary>
+        /// 处理数据
+        /// </summary>
+        private void executeRecord()
+        {
+            try
+            {
+                //每十分钟操作一次
+                var interval = Convert.ToInt32(ConfigurationManager.AppSettings["Log_ExecuteRecordInterval"]);
+                var mt = new Timer(interval);
+                mt.Enabled = true;
+                mt.Elapsed += (o, e) =>
+                {
+                    var t = (Timer)o;
+                    t.AutoReset = false;
+                    t.Enabled = false;
+                    t.Stop();
+                    executeRecordInner();
+                    t.Enabled = true;
+                    t.AutoReset = true;
+                    t.Start();
+                };
+            }
+            catch (Exception ex)
+            {
+                TextLoggingService.Error(ex.Message);
+            }
+        }
+
+        private void executeRecordInner()
+        {
+        }
+
         #endregion
     }
 }
