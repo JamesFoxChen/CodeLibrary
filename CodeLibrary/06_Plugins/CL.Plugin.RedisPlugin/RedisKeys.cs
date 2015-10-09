bcf4650de381f3906de39e6049c27a2877c08007
JamesFoxChen <25717594@qq.com>
2015-10-09 16:15:31 +0800

    Redis

---
+++ CodeLibrary/06_Plugins/CL.Plugin.RedisPlugin/RedisKeys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CL.Plugin.RedisPlugin
+{
+    public class RedisKeys
+    {
+        /// <summary>
+        //  当前用户信息(0}为ID
+        /// </summary>
+        public const string CurrentUserInfo = "CurrentUser:{0}";
+    }
+}
---
+++ CodeLibrary/08_Tests/CL.Test.Demo/Thrid/Redis/RedisTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CL.Plugin.Qiniu;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CL.Plugin.RedisPlugin;
+
+namespace CL.Test.Demo.Thrid.Redis
+{
+    [TestClass]
+    public class RedisTest
+    {
+        [TestMethod]
+        public void 基础测试()
+        {
+            RedisBase.Item_Set<string>("aa", DateTime.Now.ToString());
+            var value = RedisBase.Item_Get<string>("aa");
+        }
+    }
+}
