1c452ba16a647bfca2d882f7fe00cf845d95f689
JamesFoxChen <25717594@qq.com>
2015-10-19 14:49:27 +0800

    redis

---
+++ CodeLibrary/06_Plugins/CL.Plugin.RedisPlugin/RedisConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Configuration;
+using ServiceStack.Redis;
+
+namespace CL.Plugin.RedisPlugin
+{
+    public class RedisConfig
+    {
+        public static RedisClient Redis
+        {
+            get
+            {
+                return (RedisClient)reidsPools.GetClient();
+            }
+        }
+
+        private static string[] hosts;
+        private static PooledRedisClientManager reidsPools;
+
+        static RedisConfig()
+        {
+            var hosts = ConfigurationManager.AppSettings["RedisServer"].Split(',');
+
+            reidsPools = new PooledRedisClientManager(hosts, hosts, new RedisClientManagerConfig
+            {
+                MaxWritePoolSize = 100,//“写”链接池链接数
+                MaxReadPoolSize = 200,//“读”链接池链接数
+                AutoStart = true,
+                DefaultDb = 0
+            });
+        }
+    }
+}
---
+++ CodeLibrary/06_Plugins/CL.Plugin.RedisPlugin/RedisHelper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CL.Plugin.RedisPlugin
+{
+    public class RedisHelper
+    {
+        /// <summary>
+        /// 获取值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public T Get<T>(string key)
+        {
+            using (var redis = RedisConfig.Redis)
+            {
+                return redis.Get<T>(key);
+            }
+        }
+
+        /// <summary>
+        /// 设置值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool Set<T>(string key, T value)
+        {
+            using (var redis = RedisConfig.Redis)
+            {
+                return redis.Set<T>(key, value);
+            }
+        }
+
+        /// <summary>
+        /// 设置值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool Set<T>(string key, T value, DateTime dt)
+        {
+            using (var redis = RedisConfig.Redis)
+            {
+                return redis.Set<T>(key, value, dt);
+            }
+        }
+
+        /// <summary>
+        /// 移除单个值
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public bool Remove(string key)
+        {
+            using (var redis = RedisConfig.Redis)
+            {
+                return redis.Remove(key);
+            }
+        }
+
+        /// <summary>
+        /// 移除集合值
+        /// </summary>
+        /// <param name="keys"></param>
+        public void RemoveList(List<string> keys)
+        {
+            using (var redis = RedisConfig.Redis)
+            {
+                foreach (var key in keys)
+                {
+                    redis.Remove(key);
+                }
+            }
+        }
+    }
+}
